2c6529ab3f1d601c935981e21247280724c7458e
lerondestroy <71708392+lerondestroy@users.noreply.github.com>
2021-04-01 20:37:53 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,63 @@
 document.querySelector('.screen-1').addEventListener('click', function(e) {
 	e.currentTarget.classList.add('start')
 
-	setTimeout(() => {
-		goTo(2)
-		var typed = new Typed('#screen-2__typed', {
-			strings: ['В городе происходит что-то странное. Найдите полковника среди людей, только он сможет обьяснить, что делать.'],
-		});
-	}, 4500);
+	goTo('2', 0)
+	e.currentTarget.classList.remove('start')
 })
 
 document.querySelector('.screen-2').addEventListener('click', function(e) {
 	if (e.target.classList.contains('screen-2__man') || e.target.closest('.screen-2__man')) {
 		e.target.closest('.screen-2__man').classList.toggle('interviewed')
 	}
+
+	if (e.target.closest('.screen-2__man--boss')) {
+		goTo('3', 0);
+	}
+})
+
+document.querySelector('.screen-3 .btn').addEventListener('click', function(e) {
+	goTo('4');
+})
+
+document.querySelector('#screen-4__fall').addEventListener('click', function(e) {
+	goTo('4-1');
+})
+
+document.querySelector('#btn-to-5').addEventListener('click', function(e) {
+	goTo('5');
+})
+
+document.querySelector('.screen-5 .btn').addEventListener('click', function(e) {
+	goTo('6');
 })
 
 
 
-function goTo(screen) {
-	document.querySelectorAll('.screen').forEach(el => {
-		el.classList.remove('active');
-	});
-	document.querySelector(`.screen-${screen}`).classList.add('active')
+$('.go-home').on('click', function () {
+	goTo('1');
+});
+
+
+
+
+function goTo(screen, delay = 0) {
+	let 	screenActive = document.querySelector(`.screen-${screen}`),
+			screenAbout = screenActive.dataset.about;
+
+
+	setTimeout(() => {
+
+		document.querySelectorAll('.screen').forEach(el => {
+			el.classList.remove('active');
+		});
+		screenActive.classList.add('active')
+		let typed = new Typed(`#screen-${screen}__typed`, {
+			strings: [screenAbout],
+			onComplete() {
+				screenActive.classList.add('show-btn')
+			}
+		});
+		console.log(typed)
+		
+	}, delay);
 }
